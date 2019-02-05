d6684c08ac0260f6c406215797f7316ae1255c84
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-02-06 02:54:12 +0530

    Injecting the Service using dependency Injection (Constructor Injection)

--- APM-Start/src/app/product/product-list.component.ts
+++ APM-Start/src/app/product/product-list.component.ts
@@ -8,40 +8,26 @@ import { ProductService } from './product.service';
   providers:[ProductService]
 })
 export class ProductListComponent implements OnInit{
+    constructor(private productService:ProductService){
+      // this automatically injects the object of ProductService into the ProductListComponent
+    }
     productPageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number =2;
     showImage: boolean = false;
     listFilter:string='cart';
-    products: IProduct[] =  [  // We use any[] in TS if we don't know what  data type to use
-            {
-              "productId": 1,
-              "productName": "Leaf Rake",
-              "productCode": "GDN-0011",
-              "releaseDate": "March 19, 2016",
-              "description": "Leaf rake with 48-inch wooden handle.",
-              "price": 19.95,
-              "starRating": 3.2,
-              "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-            },
-            {
-              "productId": 2,
-              "productName": "Garden Cart",
-              "productCode": "GDN-0023",
-              "releaseDate": "March 18, 2016",
-              "description": "15 gallon capacity rolling garden cart",
-              "price": 32.99,
-              "starRating": 4.2,
-              "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-            }
-                    ];
-
+    products: IProduct[] ;
       toggleImage():void {
         this.showImage = !this.showImage;
       }
       ngOnInit(): void {
-        throw new Error("Method not implemented.");
-      console.log("I am onInit method");
+        // We are using ngOnInit() method to initialize the products[] array
+        // before the component is created
+        //productService.getProducts() method returns the products we need
+        // So we can remove the hardcoding above
+        console.log("on init");
+
+        this.products = this.productService.getProducts();
       }
       // this method is called when ever an output form child component is sent to parent component
       // In this method we are just changing the productPageTitle property by appending the message 
