b2677b1dbf572dabd244fddf22b6b0979dcc246e
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-02-06 01:14:33 +0530

    Building a nested Component and Using the Nested Component (adding <pm-star> tag of child component in parent component.html )

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
+import { StarComponent } from './shared/star.component';
 @NgModule({
   // declarations are our own Components
   //that we created for our project.
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    StarComponent
   ],
   // These are third party components
   // These are Angular Modules or third party modules
