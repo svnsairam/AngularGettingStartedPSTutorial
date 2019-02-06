1ec9b3b1dfe79cebb76452af985f03a70350aea8
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-02-06 05:40:58 +0530

    Navigation and Routing Basics: Added an new component using ng g c product/product-details --flat command

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './product/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 @NgModule({
   // declarations are our own Components
   //that we created for our project.
   declarations: [
     AppComponent,
     ProductListComponent,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   // These are third party components
   // These are Angular Modules or third party modules
