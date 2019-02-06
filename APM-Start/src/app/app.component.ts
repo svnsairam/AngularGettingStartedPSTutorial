c3ba3dd11749d3cd99e9a5c9495132bf1b8f7c0f
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-02-06 06:14:54 +0530

    Typing the Routes to Actions and Placing the Views

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Sairam PluralS Angular: Getting Started';
+  pageTitle = 'Sairam PluralS Angular: Getting Started';
 }
